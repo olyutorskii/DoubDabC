432f1ab09e52c2663ace77e9f373f8260f40385c
olyutorskii <olyutorskii@gmail.com>
2017-03-11 00:09:50 +0900

    add DecimalWriter

---
+++ src/main/java/io/github/olyutorskii/doubdabc/io/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * License : The MIT License
+ * Copyright(c) 2017 olyutorskii
+ */
+
+/**
+ * Extended I/O-classes with Double-Dabble algorithm.
+ */
+
+package io.github.olyutorskii.doubdabc.io;
+
+/* EOF */
